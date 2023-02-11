ce91dd65f8f3e97abd85105c4cb06b5d2291781b
sulaimondawood <sulaimondawod@gmail.com>
2023-02-11 15:53:07 +0100

    New commits

---
+++ components/LatestCard.tsx
@@ -0,0 +1,5 @@
+const LatestCard = () => {
+  return <div className="bg-m-black"></div>;
+};
+
+export default LatestCard;
--- pages/store.tsx
+++ pages/store.tsx
@@ -5,7 +5,7 @@ import Shoe from "../assets/shoe.png";
 const Store = () => {
   return (
     <div className="bg-m-white">
-      <div className="flex mt-32 justify-center items-center">
+      <div className="flex mt-36 justify-center items-center">
         <h1 className="text-5xl max-w-4xl font-semibold">
           <span className="text-m-black">Store.</span>
           <span className="text-ml3-gray">
@@ -47,12 +47,12 @@ const Store = () => {
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-start">
-        <h1 className="text-ml-black">
-          The latest.{" "}
+      <div className="flex mt-36 pl-96  items-center justify-start">
+        <h1 className="text-ml-black text-3xl font-semibold">
+          The latest.
           <span className="text-ml3-gray">
-            Take a look at what's new, right now{" "}
-          </span>{" "}
+            Take a look at what's new, right now
+          </span>
         </h1>
       </div>
     </div>
