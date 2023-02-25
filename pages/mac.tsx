00b8a0185d983b608a3b2583d8a7f881d009d4b5
sulaimondawood <sulaimondawod@gmail.com>
2023-02-25 11:32:25 +0100

    added responsiveness to store

---
+++ pages/mac.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Icon from "../assets/icons/1.svg";
+import Icon2 from "../assets/icons/2.svg";
+import Icon3 from "../assets/icons/3.svg";
+import Icon4 from "../assets/icons/4.svg";
+import Icon5 from "../assets/icons/5.svg";
+import Icon6 from "../assets/icons/6.svg";
+import Icon7 from "../assets/icons/7.svg";
+import Icon8 from "../assets/icons/8.svg";
+import Icon9 from "../assets/icons/9.svg";
+import Icon10 from "../assets/icons/10.svg";
+import Icon11 from "../assets/icons/11.svg";
+import Icon12 from "../assets/icons/12.svg";
+import Icon13 from "../assets/icons/13.svg";
+import Image from "next/image";
+const Mac = () => {
+  return (
+    <div>
+      <div className="flex mt-20 gap-4 justify-center items-center">
+        <Image src={Icon} alt="icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Mac;
